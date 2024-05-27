f4a93a0325a0b231d5290270b1605e5f9ab2f775
Venkatesh Mattaparthi <vmattaparthi@evoketechnologies.com>
2024-05-27 15:50:13 +0530

    Initial Commit

--- src/main/java/com/nti/corsafe/carrier/Carrier.java
+++ src/main/java/com/nti/corsafe/carrier/Carrier.java
@@ -19,6 +19,7 @@ public class Carrier {
     private String website;
     private String email;
     private String helplineNumber;
+    @Relationship(type="operating-at")
     private List<Site> sites;
 
 }
